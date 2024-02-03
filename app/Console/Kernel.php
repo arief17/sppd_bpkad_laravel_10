2527ea023dd74f077158ac2a526d91cf942fba58
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2024-02-03 10:29:53 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:update-availability')->daily()->timezone('Asia/Jakarta');
+        $schedule->command('app:update-availability')->everyTenSeconds()->timezone('Asia/Jakarta');
     }
 
     /**
