106143cc667f124ece64f2dc98f7a0eece9f0752
Hikuroshi <hikulana04@gmail.com>
2024-03-19 21:40:20 +0700

    update

--- database/migrations/2024_12_23_144158_create_all_foreigns_table.php
+++ database/migrations/2024_12_23_144158_create_all_foreigns_table.php
@@ -120,7 +120,6 @@ return new class extends Migration
 
         Schema::table('kwitansi_perdins', function (Blueprint $table) {
             $table->foreign('kegiatan_sub_id')->references('id')->on('kegiatan_subs');
-            $table->foreign('pptk_id')->references('id')->on('pegawais');
             $table->foreign('author_id')->references('id')->on('users');
         });
 
